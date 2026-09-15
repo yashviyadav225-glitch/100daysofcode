479ad73ecb7859404f11db825289e6100885e639
yashviyadav225-glitch <yashviyadav225@gmail.com>
2026-09-15 23:54:45 +0530

    Add files via upload

---
+++ Day18_code1.c
@@ -0,0 +1,34 @@
+Q35: Write a program to print all factors of a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+1 2 3 6
+
+Input 2:
+10
+Output 2:
+1 2 5 10
+
+*/
+#include <stdio.h>
+int main() 
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factors of %d: ", n);
+    for (int i = 1; i <= n; i++) 
+    {
+        if (n % i == 0) 
+        {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
