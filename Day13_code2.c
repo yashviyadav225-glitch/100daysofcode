013940ce67d2831f4d4f8268e0301091187da774
yashviyadav225-glitch <yashviyadav225@gmail.com>
2026-09-15 22:43:14 +0530

    Add files via upload

---
+++ Day13_code2.c
@@ -0,0 +1,31 @@
+Q26: Write a program to print numbers from 1 to n.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+1 2 3 4 5
+
+Input 2:
+3
+Output 2:
+1 2 3
+
+*/
+#include <stdio.h>
+int main() 
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) 
+    {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
